30cd5516df76f8dff26ce2f75eb3579eab1f3107
harshanmahesh <98863241+harshanmahesh@users.noreply.github.com>
2022-02-09 19:41:15 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World, this is Harshan - polling test2!");
+    System.out.println("Hello World, this is Harshan - this is triggered by webhook!");
     }
 }
