c456db69fbc089e55c11d735c732340d53f6adfc
harshanmahesh <98863241+harshanmahesh@users.noreply.github.com>
2022-02-09 18:50:52 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World, this is Harshan - polling test1!");
+    System.out.println("Hello World, this is Harshan - polling test2!");
     }
 }
